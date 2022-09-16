69e8865da323cd2f3ec0eb1d7e6c8c2fb9b473ca
Felipe Lima <felipe978@hotmail.com>
2022-09-16 18:58:55 -0300

    4

--- src/Login.js
+++ src/Login.js
@@ -11,12 +11,31 @@ function Login() {
             navigate("/add");
         }
     }, [navigate])
+    async function login() {
+        console.warn(email, password)
+        let item = { email, password };
+        let result = await fetch("http://localhost:8000/api/login", {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify(item)
+        });
+        result = await result.json();
+        localStorage.setItem("user-info",JSON.stringify(result))
+        navigate("/add"); 
+
+    }
     return (
         <div>
             <Header />
             <h1>Login</h1>
-            <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} className="form-control" placeholder="E-mail"></input><br />
-            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="form-control" placeholder="Senha"></input><br />
+            <div className='col-sm-6 offset-sm-3'>
+                <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} className="form-control" placeholder="E-mail"></input><br />
+                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="form-control" placeholder="Senha"></input><br />
+                <button onClick={login} className='btn btn-primary'>Login</button>
+            </div>
         </div>
     )
 }
